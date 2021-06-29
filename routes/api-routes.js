4f8c91337c1c09a6d5545fa1346f40cb4a56db2f
LB Vik <laura.bullek@gmail.com>
2021-06-29 19:05:41 -0400

    Created module export to display the notes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,6 +4,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Displays the notes
+module.exports = function(app) {  
+  app.get("/api/notes", function(req, res) {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+    res.json(JSON.parse(data));})
+  });
 
 
 // Completed with help from my tutor, Kristy V. from https://www.wyzant.com/
