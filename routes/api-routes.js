580795e58b4114091272eff891041241abf65edc
LB Vik <laura.bullek@gmail.com>
2021-06-29 19:02:57 -0400

    Installed Express packages

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,2 +1,9 @@
 const db = require("../db/db.json");
 const fs = require("fs");
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+// Completed with help from my tutor, Kristy V. from https://www.wyzant.com/
